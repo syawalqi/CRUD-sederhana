411768e09ba2a75463039a42209e7429e8ac3d7c
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-03-21 02:40:41 +0700

    com 27

--- CRUD sederhana/Form1.cs
+++ CRUD sederhana/Form1.cs
@@ -84,7 +84,7 @@ namespace CRUD_sederhana
                         return;
                     }
 
-                    conn.Open();
+                    conn.Open(); // Membuka koneksi ke SQL Server
                     string query = "INSERT INTO Mahasiswa (NIM, Nama, Email, Telepon, Alamat) VALUES (@NIM, @Nama, @Email, @Telepon, @Alamat)";
                     using (SqlCommand cmd = new SqlCommand(query, conn))
                     {
