a88ad4c43020b2022341e8dcce0eccae7e6ec78c
syawalqi <galihmaulanasyawalqi@gmail.com>
2025-03-21 02:29:31 +0700

    comment 3

--- CRUD sederhana/Form1.cs
+++ CRUD sederhana/Form1.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Data.SqlClient;
+using System.Data.SqlClient; // Digunakan untuk koneksi ke SQL Server
 using System.Drawing;
 using System.Linq;
 using System.Text;
